0c913727add5cd79b89214d5f6088ad91c47d306
434836 <434836@CTSC00513942601.cts.com>
2016-07-28 11:48:34 +0530

    IssueID::UST16 OwnerId::1005033113 entityType::AUSRST Status::Done Reviewer::admin Comments::checking in

--- NewJavaProjectSandBox/src/Newdata.java
+++ NewJavaProjectSandBox/src/Newdata.java
@@ -3,7 +3,7 @@ public class Newdata {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("123899");
+		System.out.println("for UST16");
 	}
 
 }
