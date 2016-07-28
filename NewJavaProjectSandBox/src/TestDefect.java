d83580dfd9e4e8206fa9b2679ccbd271d2a113d7
434836 <434836@CTSC00513942601.cts.com>
2016-07-28 15:47:56 +0530

    IssueID::UST23 OwnerId::1005033113 entityType::AUSRST Status::Done Reviewer::admin Comments::Test

--- NewJavaProjectSandBox/src/TestDefect.java
+++ NewJavaProjectSandBox/src/TestDefect.java
@@ -3,7 +3,7 @@ public class TestDefect {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Fix for defect");
+System.out.println("Fix for defect123 ");
 	}
 
 }
