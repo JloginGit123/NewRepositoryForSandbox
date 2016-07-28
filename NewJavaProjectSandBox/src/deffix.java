0735f879adc22066966ab9459c44a99ceded867f
HariG <434836@CTSC00513942601.cts.com>
2016-07-28 11:30:06 +0530

    IssueID::UST11 OwnerId::1005033113 entityType::AUSRST Status::Done Reviewer::admin Comments::Test

--- NewJavaProjectSandBox/src/deffix.java
+++ NewJavaProjectSandBox/src/deffix.java
@@ -3,7 +3,7 @@ public class deffix {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Developed US_changes done by Hari");
+		System.out.println("Developed US_changes done by Hari G");
 	}
 
 }
