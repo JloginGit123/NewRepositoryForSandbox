9dae759342e83a50392d6a5eee2cbf5826371f78
Jayashree KR <jlogingit123>
2016-07-28 15:25:58 +0530

    IssueID::UST22 OwnerId::1005033113 entityType::AUSRST Status::Done Reviewer::admin Comments::Development completed

--- NewJavaProjectSandBox/src/Newdata.java
+++ NewJavaProjectSandBox/src/Newdata.java
@@ -3,7 +3,7 @@ public class Newdata {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("DEfect fix for 18");
+		System.out.println("FIX for UST");
 	}
 
 }
