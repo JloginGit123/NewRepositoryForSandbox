bcc35785a001363546f2e3f7162185ea51ad733e
Vijay Anand <Vijay.Anand2@cognizant.com>
2016-08-02 11:46:51 +0530

    IssueID::UST27 OwnerId::1005033113 entityType::AUSRST Status::Done Reviewer::admin Comments::Fixed by Jayashree

--- NewJavaProjectSandBox/src/deffix.java
+++ NewJavaProjectSandBox/src/deffix.java
@@ -3,7 +3,7 @@ public class deffix {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Developed US_changes done by Hari G1");
+		System.out.println("Developed US_changes done by Hari G1_fix1");
 	}
 
 }
