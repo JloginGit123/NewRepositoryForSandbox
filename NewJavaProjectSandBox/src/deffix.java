4a0f185209effed4febca79417f5111a68c75afb
Vijay Anand <Vijay.Anand2@cognizant.com>
2016-08-03 19:02:49 +0530

    IssueID::DEF21 OwnerId::1005033113 entityType::Dft Status::Done Reviewer::admin Comments::Fixed by Jayashree from Vijay system

--- NewJavaProjectSandBox/src/deffix.java
+++ NewJavaProjectSandBox/src/deffix.java
@@ -3,7 +3,7 @@ public class deffix {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ust 30 Developed US_changes done by test Hari G1_fix123");
+		System.out.println("ust 30 Developed US_changes done by test Hari G1_fix123_fix for defect");
 	}
 
 }
