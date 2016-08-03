deb5cfdc86b6c8023e50d36ebd53bbae25a31d74
Vijay Anand <Vijay.Anand2@cognizant.com>
2016-08-03 19:11:32 +0530

    IssueID::DEF22 OwnerId::1005033113 entityType::Dft Status::Done Reviewer::admin Comments::Defect fixed_J

--- NewJavaProjectSandBox/src/NewFile.java
+++ NewJavaProjectSandBox/src/NewFile.java
@@ -3,7 +3,7 @@ public class NewFile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("for UST17 defect fixed");
+System.out.println("Defect fix22");
 	}
 
 }
