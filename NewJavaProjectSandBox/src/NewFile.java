d4db49492a88c5f8c16dd1a6874cd7f000645710
Vijay Anand <Vijay.Anand2@cognizant.com>
2016-08-04 14:44:37 +0530

    IssueID::UST30 OwnerId::1005033113 entityType::AUSRST Status::Done Reviewer::admin Comments::Test

--- NewJavaProjectSandBox/src/NewFile.java
+++ NewJavaProjectSandBox/src/NewFile.java
@@ -3,7 +3,7 @@ public class NewFile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Defect fix19");
+System.out.println("User Story 30");
 	}
 
 }
