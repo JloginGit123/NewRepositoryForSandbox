116cf2a8db106ea995d5bd4d3b83919d4196b843
Jayashree KR <jlogingit123>
2016-08-08 15:09:18 +0530

    IssueID::UST37 OwnerId::1005033113 entityType::AUSRST Status::Done Reviewer::admin Comments::Developement completed

--- NewJavaProjectSandBox/src/deffix.java
+++ NewJavaProjectSandBox/src/deffix.java
@@ -3,7 +3,7 @@ public class deffix {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("DEveloping US31");
+		System.out.println("DEveloping US3 test");
 	}
 
 }
