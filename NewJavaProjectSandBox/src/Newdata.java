694f92bcfd5dee95cee93d3d399ca0b954c1571b
434836 <434836@CTSC00513942601.cts.com>
2016-08-17 17:57:47 +0530

    IssueID::UST46 OwnerId::1005033113 entityType::AUSRST Status::Done Reviewer::admin Comments::Jayashree_commitcheck

--- NewJavaProjectSandBox/src/Newdata.java
+++ NewJavaProjectSandBox/src/Newdata.java
@@ -3,7 +3,7 @@ public class Newdata {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("FIX for UST 301212");
+		System.out.println("Changes done for UST 46");
 	}
 
 }
