6a94b5cc324c977a94e46fd402cb6590552e0067
434836 <434836@CTSC00513942601.cts.com>
2016-08-18 10:14:42 +0530

    IssueID::UST40 OwnerId::1005033113 entityType::AUSRST Status::Done Reviewer::admin Comments::Test

--- NewJavaProjectSandBox/src/Newdata.java
+++ NewJavaProjectSandBox/src/Newdata.java
@@ -3,7 +3,7 @@ public class Newdata {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Changess done for UST666 and none");
+		System.out.println("Changess done for UST666 and");
 	}
 
 }
