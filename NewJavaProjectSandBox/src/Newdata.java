244e0fb4b1bb66e8318ff3992d477c8e464b19f1
Vijay Anand <Vijay.Anand2@cognizant.com>
2016-08-19 10:30:50 +0530

    Test commit_Vijay2

--- NewJavaProjectSandBox/src/Newdata.java
+++ NewJavaProjectSandBox/src/Newdata.java
@@ -4,6 +4,7 @@ public class Newdata {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Changess done for UST666 and");
+		System.out.println("Changes done in another file");
 	}
 
 }
