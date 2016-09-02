e8cab5a8ed4df481cee31fdb5b31bdb129f8a955
Jayashree KR <jlogingit123>
2016-09-02 13:51:32 +0530

    IssueID::UST6 OwnerId::2222255764 entityType::AUSRST Status::Done Reviewer::admin Comments::Fixed

--- NewJavaProjectSandBox/src/deffix.java
+++ NewJavaProjectSandBox/src/deffix.java
@@ -7,7 +7,7 @@ public class deffix {
 		System.out.println("TEst multilple login_Vijay");
 		System.out.println("TEst multilple login_Hari for UST60.");
 		System.out.println("TEst multilple login_Vijay 61.");
-		System.out.println("TEst multilple login_Prashanth UST6.");
+		System.out.println("TEst multilple login_Prashanth UST6_new.");
 	}
 
 }
