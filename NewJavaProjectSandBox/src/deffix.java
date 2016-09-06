06062eb208268f1f118f8853d797b578ff10c887
146522 <146522@CTSC00513942601.cts.com>
2016-09-06 13:26:41 +0530

    IssueID::DEF7 OwnerId::2222255764 entityType::Dft Status::Done Reviewer::admin Comments::resolved the defect

--- NewJavaProjectSandBox/src/deffix.java
+++ NewJavaProjectSandBox/src/deffix.java
@@ -11,6 +11,7 @@ public class deffix {
 		System.out.println("TEst multilple login_Prashanth Def4");
 		System.out.println("TEst multilple login_dipanjan changes");
 		System.out.println("TEst multilple login_dipanjan changes0609");
+		System.out.println("TEst multilple login_dipanjan changes0609_defect");
 	}
 
 }
