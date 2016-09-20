b1d9b054810c73035304038d640c84a8169d954f
Hari G <Hari.Gurunathan@cognizant.com>
2016-09-20 12:44:39 +0530

    IssueID::UST28 OwnerId::2222255764 entityType::AUSRST Status::Done Reviewer::admin Comments::Test

--- NewJavaProjectSandBox/src/deffix.java
+++ NewJavaProjectSandBox/src/deffix.java
@@ -21,6 +21,7 @@ public class deffix {
 		System.out.println("TEst multilple login_hari change on 1409");
 		System.out.println("TEst multilple login_hari change on 1409");
 		System.out.println("TEst multilple login_hari change on 1409");
+		System.out.println("TEst multilple login_hari change on 1406");
 	}
 
 }
