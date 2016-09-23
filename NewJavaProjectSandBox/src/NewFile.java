9f4753677db5650a88f8176c1a7132d427cdaa70
Hari G <Hari.Gurunathan@cognizant.com>
2016-09-23 10:35:14 +0530

    IssueID::UST40 OwnerId::2222255764 entityType::AUSRST Status::Done Reviewer::admin Comments::commit for UST 40

--- NewJavaProjectSandBox/src/NewFile.java
+++ NewJavaProjectSandBox/src/NewFile.java
@@ -3,7 +3,7 @@ public class NewFile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("for UST37 fixed123");
+System.out.println("for UST40 fixed123");
 	}
 
 }
