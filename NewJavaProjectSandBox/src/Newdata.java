0ce76e2bc245f9a65793515f38e5b50158b945cb
Hari G <Hari.Gurunathan@cognizant.com>
2016-10-12 16:29:21 +0530

    IssueID::UST58 OwnerId::2222255764 entityType::AUSRST Status::Done Reviewer::admin Comments::Commit UST58-J

--- NewJavaProjectSandBox/src/Newdata.java
+++ NewJavaProjectSandBox/src/Newdata.java
@@ -8,7 +8,7 @@ public class Newdata {
 
 		System.out.println("Changes done in another file again");
 		System.out.println("Changes done in another file again UST55");
-		System.out.println("Changes done in another file again D");
+		System.out.println("Changes done in another file again UST58");
 		
 	}
 
