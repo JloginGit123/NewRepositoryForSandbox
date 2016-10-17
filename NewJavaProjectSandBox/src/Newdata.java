26bd3a83567a37e9c94e566c1921ee90a7383647
Hari G <Hari.Gurunathan@cognizant.com>
2016-10-17 17:14:34 +0530

    IssueID::DEF36 OwnerId::2222255764 entityType::Dft Status::Done Reviewer::admin Comments::Def36

--- NewJavaProjectSandBox/src/Newdata.java
+++ NewJavaProjectSandBox/src/Newdata.java
@@ -9,7 +9,7 @@ public class Newdata {
 		System.out.println("Changes done in another file again");
 		System.out.println("Changes done in another file again UST55");
 		System.out.println("Changes done in another file again UST66");
-		
+		System.out.println("Changes done in another file again DEF36")
 	}
 
 }
