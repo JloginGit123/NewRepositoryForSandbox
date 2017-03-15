727d232e3a1ccecdb4a1d6fd147bec2cc14352ef
Hari G <Hari.Gurunathan@cognizant.com>
2017-03-15 16:31:28 +0530

    IssueID::DEF70 OwnerId::2222255764 entityType::Dft Status::Done Reviewer::admin Comments::Test

--- NewJavaProjectSandBox/src/Userstory.java
+++ NewJavaProjectSandBox/src/Userstory.java
@@ -8,6 +8,8 @@ public class Userstory {
 		System.out.println("User for DEF44");
 		System.out.println("User for UST 124_update");
 		System.out.println("User for UST 126");
+		System.out.println("Fix for DEF DEF70");
+
 	}
 
 }
