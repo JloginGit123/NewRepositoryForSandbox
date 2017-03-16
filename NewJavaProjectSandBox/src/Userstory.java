80bb9ab08eb824fbedf407ac77ca44767f27bc07
Hari G <Hari.Gurunathan@cognizant.com>
2017-03-16 13:47:44 +0530

    IssueID::UST130 OwnerId::2222255764 entityType::AUSRST Status::Done Reviewer::admin Comments::Test

--- NewJavaProjectSandBox/src/Userstory.java
+++ NewJavaProjectSandBox/src/Userstory.java
@@ -10,7 +10,7 @@ public class Userstory {
 		System.out.println("User for UST 126");
 		System.out.println("Fix for DEF DEF70");
 		System.out.println("Implementation User story UST128");
-
+		System.out.println("Implementation User story UST130");
 	}
 
 }
