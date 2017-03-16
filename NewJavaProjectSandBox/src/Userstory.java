3eb1006b184ceba82f8718cb3ff5fefd35639b02
Hari G <Hari.Gurunathan@cognizant.com>
2017-03-16 09:46:15 +0530

    IssueID::UST128 OwnerId::2222255764 entityType::AUSRST Status::Done Reviewer::admin Comments::Test

--- NewJavaProjectSandBox/src/Userstory.java
+++ NewJavaProjectSandBox/src/Userstory.java
@@ -9,7 +9,7 @@ public class Userstory {
 		System.out.println("User for UST 124_update");
 		System.out.println("User for UST 126");
 		System.out.println("Fix for DEF DEF70");
-		System.out.println("Implement User story UST128");
+		System.out.println("Implementation User story UST128");
 
 	}
 
